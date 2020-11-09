3ef9fed1518c35d4b6d4b477fa845ede991cf894
Piotr Walen <walen.piotr@gmail.com>
2020-11-09 18:46:07 +0100

    Added eslint and prettier

--- src/firebase/initialize.ts
+++ src/firebase/initialize.ts
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
-function initializeFirebase() {
+function initializeFirebase(): void {
   const firebaseConfig = {
-    apiKey: "AIzaSyAUmxnSaesV8QddfsKO_DjGlh8amtPFiQc",
-    authDomain: "board-dbc21.firebaseapp.com",
-    databaseURL: "https://board-dbc21.firebaseio.com",
-    projectId: "board-dbc21",
-    storageBucket: "board-dbc21.appspot.com",
-    messagingSenderId: "1044659056526",
-    appId: "1:1044659056526:web:4fc21eff793638648dfedf"
+    apiKey: 'AIzaSyAUmxnSaesV8QddfsKO_DjGlh8amtPFiQc',
+    authDomain: 'board-dbc21.firebaseapp.com',
+    databaseURL: 'https://board-dbc21.firebaseio.com',
+    projectId: 'board-dbc21',
+    storageBucket: 'board-dbc21.appspot.com',
+    messagingSenderId: '1044659056526',
+    appId: '1:1044659056526:web:4fc21eff793638648dfedf',
   };
   firebase.initializeApp(firebaseConfig);
 }
